a954e06ca575e9f6b0c9dba618e543cba43161e6
AG2048 <45613281+AG2048@users.noreply.github.com>
2023-08-04 15:02:17 -0400

    Implemented recording users' role when leaving

---
+++ cogs/moderation/leaving_member_role_logging_and_regiving.py
@@ -0,0 +1,68 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord import app_commands
+from discord.app_commands import Choice
+from discord.ext import commands
+from typing import Optional
+import csv
+import datetime
+
+
+load_dotenv()
+SERVER_ID = int(os.getenv('SERVER_ID'))
+
+
+class LeavingMemberRoleLoggingAndRegivingCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.moderation_dir = None
+        self.left_users_roles_csv_full_path = None
+        self.users_ids_roles_ids = {}
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """
+        On ready, check of a role logging file exists, if not, create it.
+        """
+        curr_dir = os.path.abspath(os.path.dirname(__file__))
+        self.moderation_dir = os.path.join(curr_dir, '..', '..', 'data', 'moderation')
+        self.left_users_roles_csv_full_path = os.path.join(self.moderation_dir, 'left_users_roles.csv')
+        if not os.path.isfile(self.left_users_roles_csv_full_path):
+            # File does not exist
+            os.makedirs(self.moderation_dir, exist_ok=True)
+            with open(self.left_users_roles_csv_full_path, 'w') as file:
+                # Write header: user_id, role_ids
+                file.write('user_id,role_ids\n')
+        with open(self.left_users_roles_csv_full_path, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                self.users_ids_roles_ids[row[0]] = [int(role_id) for role_id in row[1].split(',')]
+        print('LeavingMemberRoleLoggingCog is ready.')
+
+    @commands.Cog.listener()
+    async def on_raw_member_remove(self, payload: discord.RawMemberRemoveEvent) -> None:
+        """
+        On member remove, log the member's roles and the time of removal.
+        """
+        user_id = payload.user.id
+        role_ids = [role.id for role in payload.user.roles]
+        self.users_ids_roles_ids[user_id] = role_ids
+        with open(self.left_users_roles_csv_full_path, 'a') as file:
+            file.write(f'{user_id},"{",".join([str(role_id) for role_id in role_ids])}"\n')
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member) -> None:
+        """
+        On member join, check if the member has left before, if so, give them their roles back.
+        """
+        if member.id in self.users_ids_roles_ids:
+            for role_id in self.users_ids_roles_ids[member.id]:
+                await member.add_roles(discord.Object(id=role_id))
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(
+        LeavingMemberRoleLoggingAndRegivingCog(bot),
+        guilds=[discord.Object(id=SERVER_ID)])
