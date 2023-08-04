17d2c253de80ff34968f78361ffab4fbcb530b8e
AG2048 <45613281+AG2048@users.noreply.github.com>
2023-08-04 15:22:32 -0400

    Completed the member leave role logging cog

--- cogs/moderation/leaving_member_role_logging_and_regiving.py
+++ cogs/moderation/leaving_member_role_logging_and_regiving.py
@@ -24,6 +24,7 @@ class LeavingMemberRoleLoggingAndRegivingCog(commands.Cog):
     async def on_ready(self):
         """
         On ready, check of a role logging file exists, if not, create it.
+        Also load the file into memory.
         """
         curr_dir = os.path.abspath(os.path.dirname(__file__))
         self.moderation_dir = os.path.join(curr_dir, '..', '..', 'data', 'moderation')
@@ -38,16 +39,17 @@ class LeavingMemberRoleLoggingAndRegivingCog(commands.Cog):
             reader = csv.reader(file)
             next(reader)
             for row in reader:
-                self.users_ids_roles_ids[row[0]] = [int(role_id) for role_id in row[1].split(',')]
+                self.users_ids_roles_ids[int(row[0])] = [int(role_id) for role_id in row[1].split(',')]
         print('LeavingMemberRoleLoggingCog is ready.')
 
     @commands.Cog.listener()
     async def on_raw_member_remove(self, payload: discord.RawMemberRemoveEvent) -> None:
         """
         On member remove, log the member's roles and the time of removal.
+        Be sure to exclude @everyone role (the first role in the list).
         """
         user_id = payload.user.id
-        role_ids = [role.id for role in payload.user.roles]
+        role_ids = [role.id for role in payload.user.roles][1:]  # exclude @everyone
         self.users_ids_roles_ids[user_id] = role_ids
         with open(self.left_users_roles_csv_full_path, 'a') as file:
             file.write(f'{user_id},"{",".join([str(role_id) for role_id in role_ids])}"\n')
@@ -58,8 +60,11 @@ class LeavingMemberRoleLoggingAndRegivingCog(commands.Cog):
         On member join, check if the member has left before, if so, give them their roles back.
         """
         if member.id in self.users_ids_roles_ids:
+            guild = member.guild
             for role_id in self.users_ids_roles_ids[member.id]:
-                await member.add_roles(discord.Object(id=role_id))
+                print(f"giving role {role_id} to {member.display_name}")
+                role = guild.get_role(role_id)
+                await member.add_roles(role)
 
 
 async def setup(bot: commands.Bot) -> None:
