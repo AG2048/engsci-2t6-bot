937a8c6377cb30cac638c6177bd0ea6d608996db
AG2048 <45613281+AG2048@users.noreply.github.com>
2023-08-07 00:33:01 -0400

    edit_title done

--- cogs/moderation/server_rules.py
+++ cogs/moderation/server_rules.py
@@ -1232,6 +1232,137 @@ class ServerRulesCog(commands.GroupCog, name='rules'):
             await interaction.response.send_message('You need to be an administrator to use this command.',
                                                     ephemeral=True)
 
+    @app_commands.command(
+        name='edit_ruleset_title',
+        description='Edit the title of a ruleset embed.')
+    @app_commands.describe(
+        ruleset_title='The name of the ruleset.',
+        new_title='The new title of the ruleset.')
+    @app_commands.autocomplete(ruleset_title=ruleset_autocomplete)
+    @app_commands.guilds(SERVER_ID)
+    @app_commands.checks.has_any_role(*ADMINISTRATION_ROLES_IDS)
+    async def edit_ruleset_title(
+            self,
+            interaction: discord.Interaction,
+            ruleset_title: str,
+            new_title: str) -> None:
+        """
+        Edit the title of a ruleset embed.
+        This command can be used only if the server already has rules.
+        title cannot be None
+
+        Check if the server has rules, if not, send a message saying that the server does not have rules.
+        Check if server rules message still exists, if not, send a message saying that.
+        ruleset_title lets user choose which ruleset to add the field to
+        convert ruleset_title to ruleset_index
+        Access the ruleset embed from the index from message.embeds.
+        embed.title = new_title
+        Check if the ruleset embed is too long, if so, send a message saying that the ruleset embed is too long.
+        Log previous embeds.
+        Load the ruleset embed to memory.
+        Edit the server rules message to replace the old ruleset embed with the new ruleset embed.
+        Write to csv file. Since this could be in the middle of the file, we need to write the whole ruleset again.
+        """
+        possible_ruleset_titles = [f"{embed_info_dict['title']} - {i}" for i, embed_info_dict in
+                                   enumerate(self.server_rule_message_embeds_info_dict_list, 1)]
+        if ruleset_title not in possible_ruleset_titles:
+            await interaction.response.send_message('Invalid ruleset title.', ephemeral=True)
+            return
+        ruleset_index = possible_ruleset_titles.index(ruleset_title)
+        # Check if the server has rules, if not, send a message saying that the server does not have rules.
+        if not self.server_has_rule:
+            await interaction.response.send_message('Server does not have a rules message linked to the bot yet.',
+                                                    ephemeral=True)
+            return
+
+        try:
+            channel = self.bot.get_channel(self.server_rule_channel_id)
+            message = await channel.fetch_message(self.server_rule_message_id)
+        except discord.errors.NotFound:
+            await interaction.response.send_message('Server rules message not found.', ephemeral=True)
+            self.server_has_rule = False
+            return
+
+        # Set up a new array of embeds, replacing the old embed with the new embed.
+        embeds = message.embeds
+        for i, embed in enumerate(embeds):
+            if i == ruleset_index:
+                embed.title = new_title
+                embed.set_footer(
+                    text=f'Last updated by {interaction.user.name + (("#" + interaction.user.discriminator) if len(interaction.user.discriminator) > 1 else "")} at ({datetime.datetime.now().astimezone().tzinfo.tzname(datetime.datetime.now().astimezone())})')
+                embed.timestamp = datetime.datetime.now()
+
+        if embed_surpassed_limit(embeds):
+            await interaction.response.send_message(
+                'Embed limit surpassed (too many embeds or too many characters)\nThe max number of embeds is 10 and the max number of characters is 6000.',
+                ephemeral=True)
+            return
+
+        # Log previous rules message in the log channel, only if the server previously has rules
+        previous_rules_embeds = []
+        for embed_info_dict in self.server_rule_message_embeds_info_dict_list:
+            embed = discord.Embed()
+            embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar.url)
+            embed.title = embed_info_dict['title']
+            embed.description = embed_info_dict['description']
+            embed.set_thumbnail(url=embed_info_dict['thumbnail_url'])
+            # Colour is a hex string, so we convert it to a discord.Colour object. If it is None, we set it to None.
+            embed.colour = discord.Colour.from_str(embed_info_dict['colour']) if embed_info_dict[
+                'colour'] else None
+            for field in embed_info_dict['fields']:
+                embed.add_field(name=field['name'], value=field['value'], inline=False)
+            embed.set_footer(
+                # This tells us who updated the rules and when
+                text=f'Before update by {interaction.user.name + (("#" + interaction.user.discriminator) if len(interaction.user.discriminator) > 1 else "")} at ({datetime.datetime.now().astimezone().tzinfo.tzname(datetime.datetime.now().astimezone())})')
+            embed.timestamp = datetime.datetime.now()
+            previous_rules_embeds.append(embed)
+        log_channel = self.bot.get_channel(LOG_CHANNEL_ID)
+        # Send log message, mention it is a rule change.
+        await log_channel.send(content=f'**Server Rule Changed:**\n{self.server_rule_message_content}',
+                               embeds=previous_rules_embeds)
+
+        # Load new embed_field to memory.
+        editing_embed = self.server_rule_message_embeds_info_dict_list[ruleset_index]
+        editing_embed['title'] = new_title
+
+        # Edit the server rules message with the new embed message.
+        await message.edit(content=message.content, embeds=embeds)
+
+        # Write to file any changes
+        # Any None values are converted to empty strings
+        with open(self.server_rules_csv_full_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['value_name', 'value'])
+            writer.writerow(['channel_id', self.server_rule_channel_id])
+            writer.writerow(['message_id', self.server_rule_message_id])
+            writer.writerow(['message_content', self.server_rule_message_content])
+            for embed_info_dict in self.server_rule_message_embeds_info_dict_list:
+                writer.writerow(['embed_title', embed_info_dict['title'] if embed_info_dict['title'] else ''])
+                writer.writerow(
+                    ['embed_description', embed_info_dict['description'] if embed_info_dict['description'] else ''])
+                writer.writerow(['embed_thumbnail_url',
+                                 embed_info_dict['thumbnail_url'] if embed_info_dict['thumbnail_url'] else ''])
+                writer.writerow(['embed_colour', embed_info_dict['colour'] if embed_info_dict['colour'] else ''])
+                for field in embed_info_dict['fields']:
+                    writer.writerow(['embed_field_name', field['name'] if field['name'] else ''])
+                    writer.writerow(['embed_field_value', field['value'] if field['value'] else ''])
+
+        # Send a message to the user saying that the title has been updated.
+        await interaction.response.send_message('Title updated.', ephemeral=True)
+
+    @edit_ruleset_title.error
+    async def edit_ruleset_titleError(
+            self,
+            interaction: discord.Interaction,
+            error: app_commands.AppCommandError):
+        """
+        Error handler for edit_ruleset_title command.
+        Currently only handles MissingAnyRole error, where the user does not have any of the required roles.
+        """
+        if isinstance(error, app_commands.MissingAnyRole):
+            await interaction.response.send_message('You need to be an administrator to use this command.',
+                                                    ephemeral=True)
+
     # TODO: check if I spelled color or colour
     # TODO: make a write_to_csv function
 
@@ -1250,7 +1381,7 @@ class ServerRulesCog(commands.GroupCog, name='rules'):
     #       insert_new_before:
     #           --ruleset--, --field--
     #       edit:
-    #           ruleset thumbnail, ruleset title, ruleset description, ruleset colour, field
+    #           --ruleset thumbnail--, --ruleset title--, ruleset description, ruleset colour, field
     #       remove:
     #           ruleset, (WE DON'T WANT TO REMOVE TITLE) ruleset title, ruleset description, ruleset colour, field
     #     user inputs:
