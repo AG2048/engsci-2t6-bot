1cffef4b3e98f918120c353c324f3a79e549ff78
AG2048 <45613281+AG2048@users.noreply.github.com>
2023-08-07 01:04:44 -0400

    edit_message_content done

--- cogs/moderation/server_rules.py
+++ cogs/moderation/server_rules.py
@@ -1636,6 +1636,114 @@ class ServerRulesCog(commands.GroupCog, name='rules'):
         if isinstance(error, app_commands.MissingAnyRole):
             await interaction.response.send_message('You need to be an administrator to use this command.',
                                                     ephemeral=True)
+
+    @app_commands.command(
+        name='edit_message_content',
+        description='Edit the message content of the server rules message.')
+    @app_commands.describe(
+        new_content='The new message content of the server rules message.')
+    @app_commands.guilds(SERVER_ID)
+    @app_commands.checks.has_any_role(*ADMINISTRATION_ROLES_IDS)
+    async def edit_message_content(
+            self,
+            interaction: discord.Interaction,
+            new_content: str) -> None:
+        """
+        Edit the message content of the server rules message.
+        This command can be used only if the server already has rules.
+        content cannot be None. Max 2000 characters.
+
+        Check if the server has rules, if not, send a message saying that the server does not have rules.
+        Check if server rules message still exists, if not, send a message saying that.
+        Check if the new content is too long, if so, send a message saying that the new content is too long.
+        Log previous embeds and message content in the log channel.
+        Load the new content to memory.
+        Edit the server rules message with the new content, and same embeds.
+        Write to csv file. Since this could be in the middle of the file, we need to write the whole ruleset again.
+        """
+
+        # Check if the server has rules, if not, send a message saying that the server does not have rules.
+        if not self.server_has_rule:
+            await interaction.response.send_message('Server does not have a rules message linked to the bot yet.',
+                                                    ephemeral=True)
+            return
+
+        try:
+            channel = self.bot.get_channel(self.server_rule_channel_id)
+            message = await channel.fetch_message(self.server_rule_message_id)
+        except discord.errors.NotFound:
+            await interaction.response.send_message('Server rules message not found.', ephemeral=True)
+            self.server_has_rule = False
+            return
+
+        # Check if the new content is too long, if so, send a message saying that the new content is too long.
+        if len(new_content) > 2000:
+            await interaction.response.send_message('Message content is too long (max 2000 characters).', ephemeral=True)
+            return
+
+        # Log previous rules message in the log channel, only if the server previously has rules
+        previous_rules_embeds = []
+        for embed_info_dict in self.server_rule_message_embeds_info_dict_list:
+            embed = discord.Embed()
+            embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar.url)
+            embed.title = embed_info_dict['title']
+            embed.description = embed_info_dict['description']
+            embed.set_thumbnail(url=embed_info_dict['thumbnail_url'])
+            # Colour is a hex string, so we convert it to a discord.Colour object. If it is None, we set it to None.
+            embed.colour = discord.Colour.from_str(embed_info_dict['colour']) if embed_info_dict[
+                'colour'] else None
+            for field in embed_info_dict['fields']:
+                embed.add_field(name=field['name'], value=field['value'], inline=False)
+            embed.set_footer(
+                # This tells us who updated the rules and when
+                text=f'Before update by {interaction.user.name + (("#" + interaction.user.discriminator) if len(interaction.user.discriminator) > 1 else "")} at ({datetime.datetime.now().astimezone().tzinfo.tzname(datetime.datetime.now().astimezone())})')
+            embed.timestamp = datetime.datetime.now()
+            previous_rules_embeds.append(embed)
+        log_channel = self.bot.get_channel(LOG_CHANNEL_ID)
+        # Send log message, mention it is a rule change.
+        await log_channel.send(content=f'**Server Rule Changed:**\n{self.server_rule_message_content}',
+                               embeds=previous_rules_embeds)
+
+        # Load new embed_field to memory.
+        self.server_rule_message_content = new_content
+
+        # Edit the server rules message with the new embed message.
+        await message.edit(content=new_content, embeds=message.embeds)
+
+        # Write to file any changes
+        # Any None values are converted to empty strings
+        with open(self.server_rules_csv_full_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['value_name', 'value'])
+            writer.writerow(['channel_id', self.server_rule_channel_id])
+            writer.writerow(['message_id', self.server_rule_message_id])
+            writer.writerow(['message_content', self.server_rule_message_content])
+            for embed_info_dict in self.server_rule_message_embeds_info_dict_list:
+                writer.writerow(['embed_title', embed_info_dict['title'] if embed_info_dict['title'] else ''])
+                writer.writerow(
+                    ['embed_description', embed_info_dict['description'] if embed_info_dict['description'] else ''])
+                writer.writerow(['embed_thumbnail_url',
+                                 embed_info_dict['thumbnail_url'] if embed_info_dict['thumbnail_url'] else ''])
+                writer.writerow(['embed_colour', embed_info_dict['colour'] if embed_info_dict['colour'] else ''])
+                for field in embed_info_dict['fields']:
+                    writer.writerow(['embed_field_name', field['name'] if field['name'] else ''])
+                    writer.writerow(['embed_field_value', field['value'] if field['value'] else ''])
+
+        # Send a message to the user saying that the message content have been updated.
+        await interaction.response.send_message('Server rules message content updated.', ephemeral=True)
+
+    @edit_message_content.error
+    async def edit_message_contentError(
+            self,
+            interaction: discord.Interaction,
+            error: app_commands.AppCommandError):
+        """
+        Error handler for edit_message_content command.
+        Currently only handles MissingAnyRole error, where the user does not have any of the required roles.
+        """
+        if isinstance(error, app_commands.MissingAnyRole):
+            await interaction.response.send_message('You need to be an administrator to use this command.',
+                                                    ephemeral=True)
     # TODO: check if I spelled color or colour
     # TODO: make a write_to_csv function
 
