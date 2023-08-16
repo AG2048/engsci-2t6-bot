ab2d910d01940286aff680cabfee96dc1cc3c8e3
AG2048 <45613281+AG2048@users.noreply.github.com>
2023-08-16 02:19:45 -0400

    Error handling for bot.log

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class Bot(commands.Bot):
         embed.title = f'{self.user.name}{("#" + self.user.discriminator) if len(self.user.discriminator) > 1 else ""} (ID: {self.user.id})'
         log_message += f'\n\t {self.user.name}{("#" + self.user.discriminator) if len(self.user.discriminator) > 1 else ""} (ID: {self.user.id})'
         embed.timestamp = datetime.datetime.now()
-        embed.set_author(name=self.user.name, icon_url=self.user.avatar.url)
+        embed.set_author(name=self.user.name, icon_url=self.user.avatar.url if self.user.avatar is not None else self.user.default_avatar.url)
         embed.description = 'Bot is ready.'
         log_message += '\n\t Bot is ready.'
         await self.get_channel(LOG_CHANNEL_ID).send(embed=embed)
@@ -60,29 +60,41 @@ class Bot(commands.Bot):
         :param outcome: the outcome of the event (only used for user actions or errors)
         :return: None
         """
-        log_message = str(datetime.datetime.now())
-        embed = discord.Embed()
-        embed.title = f'{type(cog).__name__}'
-        embed.timestamp = datetime.datetime.now()
-        embed.set_author(name=self.user.name, icon_url=self.user.avatar.url)
-        if user is not None:
-            embed.add_field(name='User: ', value=f'{user.mention} ({user.id})', inline=False)
-            embed.set_footer(text=f'{user.name}{("#"+user.discriminator) if len(user.discriminator) > 1 else ""} ({user.id})', icon_url=user.avatar.url)
-            log_message += f'\n\t User: {user.name}{("#"+user.discriminator) if len(user.discriminator) > 1 else ""} ({user.id})'
-        if user_action is not None:
-            embed.add_field(name='User Action: ', value=user_action, inline=False)
-            log_message += f'\n\t User Action: {user_action}'
-        if channel is not None:
-            embed.add_field(name='In Channel: ', value=f'{channel.mention} ({channel.id})', inline=False)
-            log_message += f'\n\t In Channel: {channel.name} ({channel.id})'
-        if event is not None:
-            embed.add_field(name='Event: ', value=event, inline=False)
-            log_message += f'\n\t Event: {event}'
-        if outcome is not None:
-            embed.add_field(name='Outcome: ', value=outcome, inline=False)
-            log_message += f'\n\t Outcome: {outcome}'
-        await self.get_channel(LOG_CHANNEL_ID).send(embed=embed)
-        print(log_message)
+        try:
+            log_message = str(datetime.datetime.now())
+            embed = discord.Embed()
+            embed.title = f'{type(cog).__name__}'
+            embed.timestamp = datetime.datetime.now()
+            embed.set_author(name=self.user.name, icon_url=self.user.avatar.url if self.user.avatar is not None else self.user.default_avatar.url)
+            if user is not None:
+                embed.add_field(name='User: ', value=f'{user.mention} ({user.id})', inline=False)
+                embed.set_footer(text=f'{user.name}{("#"+user.discriminator) if len(user.discriminator) > 1 else ""} ({user.id})', icon_url=user.avatar.url if user.avatar is not None else user.default_avatar.url)
+                log_message += f'\n\t User: {user.name}{("#"+user.discriminator) if len(user.discriminator) > 1 else ""} ({user.id})'
+            if user_action is not None:
+                embed.add_field(name='User Action: ', value=user_action, inline=False)
+                log_message += f'\n\t User Action: {user_action}'
+            if channel is not None:
+                embed.add_field(name='In Channel: ', value=f'{channel.mention} ({channel.id})', inline=False)
+                log_message += f'\n\t In Channel: {channel.name} ({channel.id})'
+            if event is not None:
+                embed.add_field(name='Event: ', value=event, inline=False)
+                log_message += f'\n\t Event: {event}'
+            if outcome is not None:
+                embed.add_field(name='Outcome: ', value=outcome, inline=False)
+                log_message += f'\n\t Outcome: {outcome}'
+            await self.get_channel(LOG_CHANNEL_ID).send(embed=embed)
+            print(log_message)
+        except Exception as e:
+            log_message = str(datetime.datetime.now())
+            log_message += f'\n\t Error: {e}'
+            log_message += f'\n\t cog: {cog}'
+            log_message += f'\n\t user: {user}'
+            log_message += f'\n\t user_action: {user_action}'
+            log_message += f'\n\t channel: {channel}'
+            log_message += f'\n\t event: {event}'
+            log_message += f'\n\t outcome: {outcome}'
+            print(log_message)
+            await self.get_channel(LOG_CHANNEL_ID).send(log_message)
 
 
 bot = Bot()
